6e9f9ebe14b3fca3a416a4f0379cae41f2bc07a7
Carlos Alexandre <168609677+Ominitriix@users.noreply.github.com>
2024-12-15 17:01:36 -0300

    ajuste da calculadora

--- calculadora/assets/main.js
+++ calculadora/assets/main.js
@@ -5,24 +5,35 @@ const btn_calcular = document.querySelector("#calc_button_calcular");
 const btn_limpar = document.querySelector("#calc_button_limpar");
 const resultado = document.querySelector("#res");
 
+// Confere se tem letra
+text_area.addEventListener("input", () => {
+  let verificarValue = input.value;
+  //
+  if (isNaN(verificarValue)) {
+    //Instrução para remover caractere
+    const removeCarac = input.value.slice(0, -1);
+    input.value = removeCarac;
+  }
+});
+
 btn_number.forEach((button, index) => {
   // uso o proprio index para servir como numero para as somas
   let i = ++index;
 
   button.addEventListener("click", () => {
+    let firstNumber = input.value;
+
     // Valida se o ultimo botao foi clicado
     if (i >= 10) {
-      // Confere se tem mas de um numero
-      if (input.value.length <= 1) {
+      // Confere se o primeiro numero é 0
+      if (firstNumber == "0") {
         input.value = 0;
       } else {
         input.value += 0;
       }
     } else {
       // Basicamente não deixo o 0 se repetir sendo o primeiro caractere
-      let firstNumber = input.value.substr(0, 1);
-
-      if (Number(firstNumber) === 0) {
+      if (firstNumber == "0") {
         input.value = i;
       } else {
         input.value += i;
@@ -33,27 +44,34 @@ btn_number.forEach((button, index) => {
 
 btn_sinal.forEach((button, index) => {
   button.addEventListener("click", () => {
-    switch (index) {
-      case 0:
-        input.value += "+";
-        break;
-      case 1:
-        input.value += "-";
-        break;
-      case 2:
-        input.value += "*";
-        break;
-      case 3:
-        input.value += "/";
-        break;
-      case 4:
-        input.value += "%";
-        break;
-      case 5:
-        input.value += ".";
-        break;
-      default:
-        input.value = "Error";
+    if (input.value.length > 0) {
+      switch (index) {
+        case 0:
+          input.value += "+";
+          break;
+        case 1:
+          input.value += "-";
+          break;
+        case 2:
+          input.value += "*";
+          break;
+        case 3:
+          input.value += "/";
+          break;
+        case 4:
+          input.value += "%";
+          break;
+        case 5:
+          const removeCarac = input.value.slice(0, -1);
+          input.value = removeCarac;
+          resultado.innerHTML = "";
+          break;
+        case 6:
+          input.value += ".";
+          break;
+        default:
+          input.value = "Error";
+      }
     }
   });
 });
@@ -63,12 +81,10 @@ btn_limpar.addEventListener("click", () => {
   resultado.textContent = "";
 });
 
-btn_calcular.addEventListener("click", Soma);
-
 function Soma() {
   try {
     // Captura a expressão
-    let exp = /\d+|\*|\/|\+|\-/g;
+    let exp = /\d+([.,]\d)?|\*|\/|\+|\-|\%/g;
     let arrayValues = input.value.match(exp);
 
     // Avisa caso o campo esteja vazio
@@ -85,7 +101,7 @@ function Soma() {
       // Identifica e calcula
       switch (operador) {
         case "*":
-          res *= numero;
+          res += numero;
           break;
         case "/":
           res /= numero;
@@ -95,6 +111,9 @@ function Soma() {
           break;
         case "-":
           res -= numero;
+          break;
+        case "%":
+          res = (parseFloat(arrayValues[i - 1]) * numero) / 100;
           break;
 
         default:
@@ -108,3 +127,5 @@ function Soma() {
     console.log(error);
   }
 }
+
+btn_calcular.addEventListener("click", Soma);
